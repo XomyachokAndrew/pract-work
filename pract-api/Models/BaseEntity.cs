03dd7b43cb33e95ae0e3de89259dadc12a98b4be
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-04 16:07:38 +0300

    Added BaseEntity model

---
+++ pract-api/Models/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pract.Models
+{
+    public abstract class BaseEntity
+    {
+        [Column("crated_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Column("is_deleted")]
+        public bool IsDeleted { get; set; }
+    }
+}
