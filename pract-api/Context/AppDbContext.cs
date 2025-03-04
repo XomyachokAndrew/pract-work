181d5621375ddf744de3b229c6e3616c7b8345d5
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-04 16:08:04 +0300

    Added Context for database

---
+++ pract-api/Context/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pract.Models;
+
+namespace Pract.Context
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public override int SaveChanges()
+        {
+            var addedOrModifiedEntries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is BaseEntity && (
+                        e.State == EntityState.Added
+                        || e.State == EntityState.Modified));
+
+            foreach (var entry in addedOrModifiedEntries)
+            {
+                var entity = (BaseEntity)entry.Entity;
+                entity.UpdatedAt = DateTime.UtcNow;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entity.CreatedAt = DateTime.UtcNow;
+                }
+            }
+
+            var deletedEntries = ChangeTracker
+                .Entries()
+                .Where(e => e.State == EntityState.Deleted && e.Entity is BaseEntity);
+
+            foreach (var entry in deletedEntries)
+            {
+                entry.State = EntityState.Modified;
+
+                var entity = (BaseEntity)entry.Entity;
+                entity.IsDeleted = true;
+                entity.UpdatedAt = DateTime.UtcNow;
+            }
+
+            return base.SaveChanges();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Глобальный фильтр для исключения удалённых записей
+            modelBuilder.Entity<Worker>().HasQueryFilter(c => !c.IsDeleted);
+            modelBuilder.Entity<Office>().HasQueryFilter(o => !o.IsDeleted);
+            modelBuilder.Entity<Post>().HasQueryFilter(o => !o.IsDeleted);
+        }
+    }
+}
