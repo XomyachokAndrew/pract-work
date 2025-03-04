f1ccbf256f1dce1b702bb32f97ba83620c4ce093
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-04 16:08:57 +0300

    Added offices controller

---
+++ pract-api/Controllers/OfficesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pract.Context;
+using Pract.Models;
+
+namespace Pract.Controllers
+{
+    [Route("api/offices")]
+    [ApiController]
+    public class OfficesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public OfficesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Offices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Office>>> GetOffices()
+        {
+            return await _context.Offices.ToListAsync();
+        }
+
+        // GET: api/Offices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Office>> GetOffice(Guid id)
+        {
+            var office = await _context.Offices.FindAsync(id);
+
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            return office;
+        }
+
+        // PUT: api/Offices/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOffice(Guid id, Office office)
+        {
+            if (id != office.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(office).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OfficeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Offices
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Office>> PostOffice(Office office)
+        {
+            _context.Offices.Add(office);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOffice", new { id = office.Id }, office);
+        }
+
+        // DELETE: api/Offices/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOffice(Guid id)
+        {
+            var office = await _context.Offices.FindAsync(id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            _context.Offices.Remove(office);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OfficeExists(Guid id)
+        {
+            return _context.Offices.Any(e => e.Id == id);
+        }
+    }
+}
