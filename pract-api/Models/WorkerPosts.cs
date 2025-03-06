86c82cfacc37f30ffb9fd1c9cc4ac626751f3f84
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-06 11:24:35 +0300

    Added WorkerOffices and WorkerPosts

---
+++ pract-api/Models/WorkerPosts.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pract.Models
+{
+    [Table("worker_posts")]
+    public class WorkerPosts : BaseEntity
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("worker_id")]
+        public Guid WorkerId { get; set; }
+
+        [Column("post_id")]
+        public Guid PostId { get; set; }
+
+        [ForeignKey("PostId")]
+        public Post Post { get; set; }
+
+        [ForeignKey("WorkerId")]
+        public Worker Worker { get; set; }
+    }
+}
