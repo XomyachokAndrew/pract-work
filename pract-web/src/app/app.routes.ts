85eabce1fbe3192993b3c50fdc6275f88d882d43
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-10 16:23:02 +0300

    Added path '/workers'

--- pract-web/src/app/app.routes.ts
+++ pract-web/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { WorkersComponent } from '@pages/workers/workers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "/workers", component: WorkersComponent},
+];
