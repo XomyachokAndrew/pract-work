c03e43d8bc3e9a652d063617d562c99d03388588
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-10 16:18:41 +0300

    Added page worker

---
+++ pract-web/src/app/pages/workers/workers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workers',
+  imports: [],
+  templateUrl: './workers.component.html',
+  styleUrl: './workers.component.less'
+})
+export class WorkersComponent {
+
+}
