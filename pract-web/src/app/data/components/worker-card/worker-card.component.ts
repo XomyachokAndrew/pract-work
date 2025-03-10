2f6e4c9a3afebc3ffa2474703444c6efaf612bf0
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-10 16:19:00 +0300

    Added worker card

---
+++ pract-web/src/app/data/components/worker-card/worker-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-worker-card',
+  imports: [],
+  templateUrl: './worker-card.component.html',
+  styleUrl: './worker-card.component.less'
+})
+export class WorkerCardComponent {
+
+}
