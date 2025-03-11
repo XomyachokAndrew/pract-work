4ea18c3cc20f63079f0bcc7e200ed6db12178118
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-11 09:43:21 +0300

    Added method

--- pract-web/src/app/data/services/workers.service.ts
+++ pract-web/src/app/data/services/workers.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { WorkerWithDetailsDto } from '@models/workers-dtos';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkersService {
+  URL = `${environment.apiUrl}/workers`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWorkers(): Observable<WorkerWithDetailsDto[]> {
+    return this.http.get<WorkerWithDetailsDto[]>(this.URL).pipe(catchError(this.handleError))
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Ошибки на стороне клиента
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Ошибки на стороне сервера
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    // Возвращаем ошибку в виде Observable
+    return throwError(() => new Error(errorMessage));
+  }
 }
