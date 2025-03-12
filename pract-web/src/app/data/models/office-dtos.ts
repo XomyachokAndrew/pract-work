3aba96544f95d46e77c63c5e1f56235723ff4db7
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-12 09:00:51 +0300

    Added office dtos

--- pract-web/src/app/data/models/office-dtos.ts
+++ pract-web/src/app/data/models/office-dtos.ts
@@ -3,3 +3,10 @@ export interface OfficeDto {
     name: string;
     address: string;
 }
+
+export interface OfficeHistoryDto {
+    id: string;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+}
