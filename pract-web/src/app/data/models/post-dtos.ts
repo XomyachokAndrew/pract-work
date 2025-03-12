0eaa673fa0873545345d38d5434d21170cfa229c
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-12 09:00:59 +0300

    Added post dtos

--- pract-web/src/app/data/models/post-dtos.ts
+++ pract-web/src/app/data/models/post-dtos.ts
@@ -2,3 +2,10 @@ export interface PostDto {
     id: string;
     name: string;
 }
+
+export interface PostHistoryDto {
+    id: string;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+}
