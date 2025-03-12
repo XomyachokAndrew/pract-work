c5988425618cbaa3a00c205f0c8072098252e4c3
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-12 15:38:42 +0300

    Added put method

--- pract-web/src/app/data/services/worker.service.ts
+++ pract-web/src/app/data/services/worker.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { WorkerWithDetailsDto } from '@models/workers-dtos';
+import { WorkerDto, WorkerWithDetailsDto } from '@models/workers-dtos';
 import { environment } from '@env/environment';
 
 @Injectable({
@@ -16,6 +16,19 @@ export class WorkerService {
   getWorkers(): Observable<WorkerWithDetailsDto[]> {
     return this.http
       .get<WorkerWithDetailsDto[]>(this.URL)
+      .pipe(catchError(this.handleError));
+  }
+
+  putWorker(id: string, workerName: WorkerDto): Observable<WorkerDto> {
+    const apiURL = `${this.URL}/${id}`;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http
+      .put<WorkerDto>(apiURL, workerName, httpOptions)
       .pipe(catchError(this.handleError));
   }
 
