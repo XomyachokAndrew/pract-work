328ae8685858351f4c0200e2e2ab246139413fd6
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-03-12 09:01:20 +0300

    Added click on button

--- pract-web/src/app/data/components/worker-card/worker-card.component.ts
+++ pract-web/src/app/data/components/worker-card/worker-card.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { Router } from '@angular/router';
+import { WorkerWithDetailsDto } from '@models/workers-dtos';
 import { TuiAppearance, TuiButton, TuiTitle } from '@taiga-ui/core';
-import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-worker-card',
@@ -15,5 +17,12 @@ import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
   styleUrl: './worker-card.component.less'
 })
 export class WorkerCardComponent {
+  @Input() worker!: WorkerWithDetailsDto;
 
+  constructor(private router: Router) {
+  }
+
+  onClick(workerData: WorkerWithDetailsDto) {
+    this.router.navigate(['/worker'], { state: { worker: workerData } });
+  }
 }
